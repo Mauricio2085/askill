7d2eaa3fe18f3a1e81dc7439d0f09664bd0fb06e
Mauricio2085 <maoca@gmail.com>
2024-01-03 23:20:29 -0500

    change to basic css instead of tailwind

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+
+const services = document.querySelector('#services');
+const servicesContainer = document.querySelector('.services-container');
+
+const showServices = () => {
+    servicesContainer.classList.remove('inactive');
+}
+
+const hiddeServices = () => {
+    servicesContainer.classList.add('inactive');
+
+}
+services.addEventListener('mouseenter', showServices)
+servicesContainer.addEventListener('mouseleave', hiddeServices);
+
